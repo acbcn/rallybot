0706b54f2c7e77542fb7b141d11cb8649954d766
acbcn <cannonac@live.com>
2025-02-17 17:20:07 +0000

    Update bot permissions in invite URL

--- commands/invite.js
+++ commands/invite.js
@@ -6,10 +6,16 @@ module.exports = {
     .setDescription('Get the bot\'s invite URL'),
   async execute(interaction) {
     const clientId = process.env.CLIENT_ID;
-    const inviteUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&scope=applications.commands%20bot&permissions=277025392640`;
+    // Permissions:
+    // - View Channels (1024)
+    // - Send Messages (2048)
+    // - Add Reactions (64)
+    // - Use External Emojis (262144)
+    const permissions = 1024 + 2048 + 64 + 262144; // = 265280
+    const inviteUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=${permissions}&scope=bot%20applications.commands`;
     
     await interaction.reply({
-      content: `You can invite the bot to other servers using this link:\n${inviteUrl}\n\nMake sure the bot is set as "Public Bot" in the Discord Developer Portal under "Bot" settings.`,
+      content: `You can invite the bot to other servers using this link:\n${inviteUrl}\n\nThe bot can also be added directly through Discord's App Directory.`,
       ephemeral: true
     });
   },
